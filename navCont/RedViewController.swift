97490e83280c28b06b10c322e24db1c075b306e8
West Kraemer <kraemerwest@gmail.com>
2019-01-31 13:27:56 -0500

    add pushes and unwinds to go back

---
+++ navCont/RedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RedViewController.swift
+//  navCont
+//
+//  Created by West Kraemer on 1/31/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToRed (segue: UIStoryboardSegue) {}
+
+}
---
+++ navCont/YellowViewController.swift
@@ -0,0 +1,23 @@
+//
+//  YellowViewController.swift
+//  navCont
+//
+//  Created by West Kraemer on 1/31/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backtoRed() {
+        performSegue(withIdentifier: "backToRed", sender: nil)
+    }
+
+}
